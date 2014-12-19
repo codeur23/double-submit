77cd4479c1d4b08c2f1fa64667868fab1ffde73d
mestre.stephane <mestre.stephane@gmail.com>
2014-12-19 22:50:12 +0100

    compter les erreurs de validation

--- Submit.js
+++ Submit.js
@@ -1,12 +1,27 @@
 "use strict";
 
+
+//ajout pour gérer le nbr d'erreurs remonté par le pluggin jquery de validation
+
+var nbrerrors = 0;
+
+$.validator.setDefaults({
+             showErrors: function (errorMap, errorList) {
+
+                 nbrERRORS = this.numberOfInvalids();
+                 console.log("validation avec nbrerrors = " + nbrERRORS);
+                 this.defaultShowErrors();
+             }
+});
+
+
+
 //permet d'éviter que l'on soumette deux fois 
 le formulaire par un click suivi immédiatement d'un autre click
 $(document).ready(
 
  function () {
      
-   
      try {
               
          try {
